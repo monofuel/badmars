fe1441fdfd4c0865da8051c78d6c78e5b6c45343
monofuel <monofuel34089@gmail.com>
2016-02-18 21:22:17 -0800

    add gulpfile

---
+++ client/gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var flow = require('gulp-flowtype');
+var fs = require("fs");
+
+gulp.task('default', function () {
+
+	browserify('badmars-v1/client.js')
+		.transform("babelify", {
+			presets: ["es2015", "stage-0"],
+			plugins: ["transform-flow-strip-types"]
+		})
+		.bundle()
+		.pipe(fs.createWriteStream('../public/js/badmars/badmars-v1.js'));
+})
