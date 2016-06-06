11c0cd123d83686f456b3150281aaea38d4db159
monofuel <monofuel34089@gmail.com>
2016-06-06 02:34:23 -0400

    change login message

--- client/badmars-v1/ui/login.js
+++ client/badmars-v1/ui/login.js
@@ -57,12 +57,11 @@ export var LoginModal = React.createClass({
               </li>
               <li>
                 <p>Controls: WASD to move, Q and E rotate, R and F zoom in and out</p>
-                <p>You can click units to select them, and right click them to move around. You can only move your own units</p>
+				<p>Controls suck. they need to get redone soon</p>
+                <p>Spawn system is not currently enabled</p>
                 <p>Your color is determined only the first time you log in, and will stay the same every time you reload.</p>
-  			        <p>The game might put your view at the opposite side of the map from your units. this is a bug relating to how the map loops around</p>
                 <p>Expect things to be buggy. If everything freezes, reload the page.</p>
-                <p>press Home to zoom and cycle units</p>
-                <p>Accounts are not secure (yet) all you can do is move tanks around based on your username.</p>
+                <p>Accounts are not secure (yet) login via google oauth is ready, but not enabled atm</p>
               </li>
             </ul>
           </Modal.Body>
