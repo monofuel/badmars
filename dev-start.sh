bfdfad9e74b0b7f47adaf61200c6820bf4c979d4
monofuel <monofuel@japura.net>
2016-09-18 04:40:28 -0400

    server tweaks

--- dev-start.sh
+++ dev-start.sh
@@ -8,18 +8,20 @@
 export BADMARS_DB='localhost'
 export AI_HOST='localhost'
 export MAP_HOST='localhost'
-export BADMARS_WS_SERVER='ws://localhost'
+export BADMARS_WS_SERVER=wss://japura.net
+export BADMARS_WS_PUBLIC_PORT=7006
+
 
 #start up the whole dev system in tmux
 tmux new-session -s badmars -d bash
 tmux new-window 'rethinkdb'
-tmux new-window 'cd server && nodemon chunk.js'
+tmux new-window 'cd server && supervisor chunk.js'
 sleep 10s #give it a bit to initialize the database on first run
-tmux new-window 'cd server && nodemon ai.js'
-tmux new-window 'cd server && node net.js'
-tmux new-window 'cd server && node web.js'
-tmux new-window 'cd server && nodemon pathfinder.js'
-tmux new-window 'cd server && nodemon simulate.js'
-tmux new-window 'cd server && nodemon validator.js'
+tmux new-window 'cd server && supervisor ai.js'
+tmux new-window 'cd server && supervisor net.js'
+tmux new-window 'cd server && supervisor web.js'
+tmux new-window 'cd server && supervisor pathfinder.js'
+tmux new-window 'cd server && supervisor  simulate.js'
+tmux new-window 'cd server && supervisor  validator.js'
 
 #run tmux attach after the script finishes
