c226542198fc51401b489135095dc24dbcc36d21
monofuel <monofuel@japura.net>
2017-02-04 21:27:21 -0800

    add playerUnit

---
+++ client/badmars/units/playerUnit.js
@@ -0,0 +1,43 @@
+/* @flow */
+'use strict';
+
+// monofuel
+// 2-7-2016
+
+import GroundUnit from "./groundUnit.js";
+import { PlanetLoc } from '../map/planetLoc.js';
+import { getMesh } from './unitModels.js';
+import { getSound } from '../audio/sound.js';
+import { getPlayerById } from '../net.js';
+import { updateUnit } from './unitBalance.js';
+
+export default class PlayerUnit extends GroundUnit {
+	constructor(location: PlanetLoc, playerId: string, uuid: string, type: string) {
+		if (!playerId) {
+			throw new Error('missing player');
+		}
+		if (!location) {
+			throw new Error('missing location');
+		}
+		if (!uuid) {
+			throw new Error('missing uuid');
+		}
+
+		const player = getPlayerById(playerId);
+		let color;
+		if (!player || !player.color) {
+			console.log('unknown player color');
+			color = new THREE.Color();
+		} else {
+			color = player.color;
+		}
+
+		super(location, color, type);
+
+		this.fireSound = getSound(type);
+		this.details.type = type;
+		this.uuid = uuid;
+		this.details.owner = playerId;
+		updateUnit(this);
+	}
+}
