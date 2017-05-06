28618e9a304af0a02e12cf355b86bf0a40a7e588
monofuel <monofuel@japura.net>
2017-05-06 00:11:38 -0400

    update deployment

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
-set -exuo pipefail
+set -euo pipefail
 
 export PROJECT_ID=badmars-1242
 
-#build and deploy images
-#TODO update version numbers
-docker build -t gcr.io/${PROJECT_ID}/badmars_node:v5 .
-docker build -t gcr.io/${PROJECT_ID}/badmars_go:v8 -f ./Dockerfile-go .
+if [ ! -f ./version.txt ]; then
+    echo 1 > ./version.txt
+fi
 
-gcloud docker -- push gcr.io/${PROJECT_ID}/badmars_node:v5
-gcloud docker -- push gcr.io/${PROJECT_ID}/badmars_go:v8
+VERSION=`cat ./version.txt`
+NEW_VERSION=`echo ${VERSION} + 1 | bc`
+echo "-----------------------------------------"
+echo "Current version:  ${VERSION}"
+echo "Next version:     ${NEW_VERSION}"
+echo "-----------------------------------------"
+
+echo $NEW_VERSION > ./version.txt
+
+echo "Building images"
+echo "-----------------------------------------"
+
+docker build -t gcr.io/${PROJECT_ID}/badmars_node:v${NEW_VERSION} .
+docker build -t gcr.io/${PROJECT_ID}/badmars_go:v${NEW_VERSION} -f ./Dockerfile-go .
+
+echo "-----------------------------------------"
+echo "Deploying images"
+echo "-----------------------------------------"
+gcloud docker -- push gcr.io/${PROJECT_ID}/badmars_node:v${NEW_VERSION}
+gcloud docker -- push gcr.io/${PROJECT_ID}/badmars_go:v${NEW_VERSION}
 
 cd k8s
 
-for f in *.yaml; do kubectl apply -f $f; done
+echo "-----------------------------------------"
+echo "updating pods"
+echo "-----------------------------------------"
+for f in *.yaml; do sed -e "s/\$VERSION/$VERSION/g" $f | kubectl apply -f -; done
