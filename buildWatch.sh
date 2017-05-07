740aedfdb98a8d12c0db43da05c3e00683016e0a
monofuel <monofuel@japura.net>
2017-05-07 16:53:21 -0400

    add notes

--- buildWatch.sh
+++ buildWatch.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-inotifywait -m -e modify --format '%w%f' server/nodejs | while read line
+# triggers multiple events and doesn't seem to list the filename?'
+inotifywait -m --format '%w%f' server/nodejs | while read line
 do
     echo "something happened on path $line"
+    # TODO babel needs to build the updated file and put it into the
+    # bin/server/nodejs directory in the right folder.
 done
