23a4269931c000455ccd1722fd9a4e688e4542c1
monofuel <monofuel@japura.net>
2018-03-17 15:53:46 -0700

    add flag

--- server/nodejs/db/rethinkDB/index.ts
+++ server/nodejs/db/rethinkDB/index.ts
@@ -11,110 +11,110 @@ import logger from '../../logger';
 import { createTable } from '../helper';
 import { startDBCall } from '../helper';
 
-class RethinkDB implements DB.DB {
-    public conn: r.Connection;
-    private planets: { [key: string]: Planet } = {};
-    public event: Event;
-    public session: Session;
-    public user: User;
+export class RethinkDB implements DB.DB {
+  public conn: r.Connection;
+  private planets: { [key: string]: Planet } = {};
+  public event: Event;
+  public session: Session;
+  public user: User;
 
-    private async connect(ctx: Context): Promise<void> {
-        const { env } = ctx;
-        const options: {
-            host: string,
-            db: string,
-            port?: number,
-            user?: string,
-            password?: string,
-        } = {
-                host: env.dbHost,
-                db: env.database,
-            };
-        if (env.dbPort) {
-            options.port = env.dbPort;
-        }
-        if (env.dbUser) {
-            options.user = env.dbUser;
-        }
-        if (env.dbPassword) {
-            options.password = env.dbPassword;
-        }
-        for (let i = 0; i < 5; i++) {
-            try {
-                this.conn = await r.connect(options);
-                break;
-            } catch (err) {
-                logger.trackError(null, new WrappedError(err,
-                    'failed to connect to DB' + (i < 5 ? ', retrying in 5 seconds' : 'giving up')));
-                await sleep(5000);
-            }
-        }
-        this.user = new User();
-        this.event = new Event();
-        this.session = new Session();
+  private async connect(ctx: Context): Promise<void> {
+    const { env } = ctx;
+    const options: {
+      host: string,
+      db: string,
+      port?: number,
+      user?: string,
+      password?: string,
+    } = {
+        host: env.dbHost,
+        db: env.database,
+      };
+    if (env.dbPort) {
+      options.port = env.dbPort;
+    }
+    if (env.dbUser) {
+      options.user = env.dbUser;
+    }
+    if (env.dbPassword) {
+      options.password = env.dbPassword;
+    }
+    for (let i = 0; i < 5; i++) {
+      try {
+        this.conn = await r.connect(options);
+        break;
+      } catch (err) {
+        logger.trackError(null, new WrappedError(err,
+          'failed to connect to DB' + (i < 5 ? ', retrying in 5 seconds' : 'giving up')));
+        await sleep(5000);
+      }
+    }
+    this.user = new User();
+    this.event = new Event();
+    this.session = new Session();
+  }
+
+  // setupSchema needs to be ran at least once before init() for new databases
+  // this is handled by the 'schema' container in the docker-compose setup
+  public async setupSchema(ctx: Context): Promise<void> {
+    await this.connect(ctx);
+    if ((await r.dbList().run(this.conn)).indexOf(ctx.env.database) === -1) {
+      await r.dbCreate(ctx.env.database).run(this.conn);
+    }
+    await createTable(this.conn, 'planet', 'name');
+    logger.info(ctx, 'setup rethinkdb schema');
+    await this.user.setupSchema(ctx.create(), this.conn);
+    await this.event.setupSchema(ctx.create(), this.conn);
+    await this.session.setupSchema(ctx.create(), this.conn);
+  }
+
+  public async init(ctx: Context): Promise<void> {
+    // setupSchema may have already connected
+    if (!this.conn) {
+      await this.connect(ctx);
     }
 
-    // setupSchema needs to be ran at least once before init() for new databases
-    // this is handled by the 'schema' container in the docker-compose setup
-    public async setupSchema(ctx: Context): Promise<void> {
-        await this.connect(ctx);
-        if ((await r.dbList().run(this.conn)).indexOf(ctx.env.database) === -1) {
-            await r.dbCreate(ctx.env.database).run(this.conn);
-        }
-        await createTable(this.conn, 'planet', 'name');
-        logger.info(ctx, 'setup rethinkdb schema');
-        await this.user.setupSchema(ctx.create(), this.conn);
-        await this.event.setupSchema(ctx.create(), this.conn);
-        await this.session.setupSchema(ctx.create(), this.conn);
+    const names = await this.listPlanetNames(ctx);
+    for (const planetName of names) {
+      const planet = new Planet(planetName);
+      this.planets[planetName] = planet;
+      await planet.init(ctx, this.conn);
     }
 
-    public async init(ctx: Context): Promise<void> {
-        // setupSchema may have already connected
-        if (!this.conn) {
-            await this.connect(ctx);
-        }
+    await this.user.init(ctx.create(), this.conn);
+    await this.event.init(ctx.create(), this.conn);
+    await this.session.init(ctx.create(), this.conn);
+  }
 
-        const names = await this.listPlanetNames(ctx);
-        for (const planetName of names) {
-            const planet = new Planet(planetName);
-            this.planets[planetName] = planet;
-            await planet.init(ctx, this.conn);
-        }
+  public async stop(ctx: Context): Promise<void> {
+    await this.conn.close();
+  }
 
-        await this.user.init(ctx.create(), this.conn);
-        await this.event.init(ctx.create(), this.conn);
-        await this.session.init(ctx.create(), this.conn);
-    }
+  public async createPlanet(ctx: Context, name: string, seed?: number): Promise<DB.Planet> {
+    const call = startDBCall(ctx, 'createPlanet');
+    const planet = new Planet(name, seed);
+    await planet.init(ctx, this.conn);
+    await planet.setupSchema(ctx, this.conn);
+    this.planets[name] = planet;
+    await planet.table.insert(planet.planet).run(this.conn);
+    await call.end();
+    return planet;
+  }
 
-    public async stop(ctx: Context): Promise<void> {
-        await this.conn.close();
-    }
+  public async getPlanetDB(ctx: Context, name: string): Promise<DB.Planet> {
+    // TODO if the planet is not in the map, load it from the database
+    return this.planets[name];
+  }
+  public async removePlanet(ctx: Context, name: string): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
 
-    public async createPlanet(ctx: Context, name: string, seed?: number): Promise<DB.Planet> {
-        const call = startDBCall(ctx, 'createPlanet');
-        const planet = new Planet(name, seed);
-        await planet.init(ctx, this.conn);
-        await planet.setupSchema(ctx, this.conn);
-        this.planets[name] = planet;
-        await planet.table.insert(planet.planet).run(this.conn);
-        await call.end();
-        return planet;
-    }
-
-    public async getPlanetDB(ctx: Context, name: string): Promise<DB.Planet> {
-        // TODO if the planet is not in the map, load it from the database
-        return this.planets[name];
-    }
-    public async removePlanet(ctx: Context, name: string): Promise<void> {
-        throw new Error('Method not implemented.');
-    }
-
-    public async listPlanetNames(ctx: Context): Promise<string[]> {
-        // HACK typescript doesn't think getField exists
-        const cursor = await (r.table('planet') as any).getField('name').run(this.conn);
-        const names = await cursor.toArray();
-        return names;
-    }
+  public async listPlanetNames(ctx: Context): Promise<string[]> {
+    // HACK typescript doesn't think getField exists
+    const cursor = await (r.table('planet') as any).getField('name').run(this.conn);
+    const names = await cursor.toArray();
+    return names;
+  }
 }
 const db = new RethinkDB();
 export default db;
--- server/nodejs/standalone_check.ts
+++ server/nodejs/standalone_check.ts
@@ -1,7 +1,7 @@
 require('source-map-support').install();
 import { MemoryDB } from './db/memoryDB';
 import { CheckingDB } from './db/checkingDB';
-// import RethinkDB from './db/rethinkDB';
+import { RethinkDB } from './db/rethinkDB';
 import { setupDB } from './db';
 import Standalone from './core/standalone';
 import Context from './context';
@@ -9,8 +9,12 @@ import { prepareCtx, start } from '.';
 import logger from './logger';
 
 async function init(): Promise<void> {
-  // TODO once CheckingDB is working, use it to check rethink vs memory
-  const db = new CheckingDB(new MemoryDB(), new MemoryDB());
+  const useRethink = process.env.CHECK_RETHINK === 'true';
+  console.log(`CHECK_RETHINK: ${useRethink}`);
+  const db = new CheckingDB(
+    new MemoryDB(),
+    useRethink ? new RethinkDB() : new MemoryDB(),
+  );
   setupDB(db);
   await start(await prepareCtx('standalone', db), async (ctx: Context) => {
     const standalone = new Standalone();
