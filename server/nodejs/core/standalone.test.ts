bd3033884b48f5147bed3d1704b2794e2e5d3c57
monofuel <monofuel@japura.net>
2018-08-18 17:57:36 -0700

    add test

--- server/nodejs/core/standalone.test.ts
+++ server/nodejs/core/standalone.test.ts
@@ -1,26 +1,49 @@
 require('source-map-support').install();
-import db from '../db/memoryDB';
+import memoryDB from '../db/memoryDB';
+import rethinkDB from '../db/rethinkDB';
 import { setupDB } from '../db';
 import Standalone from './standalone';
 import Context from '../context';
 import { prepareCtx } from '..';
 
 describe('start standalone', async () => {
-  let standalone: Standalone;
-  let ctx: Context;
+  describe('memoryDB', () => {
+    let standalone: Standalone;
+    let ctx: Context;
 
-  it('should setup db', async () => {
-    setupDB(db);
-    ctx = await prepareCtx('standalone', db);
+    it('should setup db', async () => {
+      setupDB(memoryDB);
+      ctx = await prepareCtx('standalone', memoryDB);
+    });
+
+    it('should start standalone', async () => {
+      standalone = new Standalone();
+      await standalone.init(ctx);
+      await standalone.start();
+    });
+
+    it('should stop standalone', async () => {
+      await standalone.stop();
+    });
   });
+  xdescribe('rethinkDB', () => {
+    let standalone: Standalone;
+    let ctx: Context;
 
-  it('should start standalone', async () => {
-    standalone = new Standalone();
-    await standalone.init(ctx);
-    await standalone.start();
-  });
+    it('should setup db', async () => {
+      setupDB(rethinkDB);
+      ctx = await prepareCtx('standalone', rethinkDB);
+    });
 
-  it('should stop standalone', async () => {
-    await standalone.stop();
+    it('should start standalone', async () => {
+      standalone = new Standalone();
+      await standalone.init(ctx);
+      await standalone.start();
+    });
+
+    it('should stop standalone', async () => {
+      await standalone.stop();
+      await rethinkDB.stop(ctx);
+    });
   });
 });
